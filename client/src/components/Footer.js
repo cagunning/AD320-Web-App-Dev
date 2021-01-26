9d99a7425c781ff20be9dad1aa71e5ce54ce7084
cagunning <shelfnunny@gmail.com>
2021-01-26 13:10:57 -0800

    creating react/express

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer height="100">
+            <div>
+                this is my footer!!
+            </div>
+
+        </footer>
+
+    )
+
+}
+
+export default Footer;
---
+++ client/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Header = () =>
+{   
+    return (
+    <header className="App-header" id="header">
+            
+            <p>
+                <img src = "https://i.kym-cdn.com/photos/images/newsfeed/001/997/104/81c.png" width="250" height="300" />
+            </p>
+            <a
+            >
+            Learning how to make a bernie app i suppose ~
+            </a>
+    </header>
+    )
+}
+
+export default Header;
